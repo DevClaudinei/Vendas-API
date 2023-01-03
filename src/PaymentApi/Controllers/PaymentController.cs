df95b5a2db696e90306152962a13324de7658883
Claudinei Santos <claudinei.santos@warren.com.br>
2023-01-03 09:19:50 -0300

    feat: inserindo a controller

---
+++ src/PaymentApi/Controllers/PaymentController.cs
@@ -0,0 +1,64 @@
+﻿using Application.Models;
+using AppServices;
+using DomainServices.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PaymentsController : ControllerBase
+{
+    private readonly IVendaAppService _vendaAppService;
+
+    public PaymentsController(IVendaAppService vendaAppService)
+    {
+        _vendaAppService = vendaAppService ?? throw new System.ArgumentNullException(nameof(vendaAppService));
+    }
+
+    [HttpPost]
+    public IActionResult Post(CreateVendaRequest vendaRequest)
+    {
+        try
+        {
+            var createdCustomer = _vendaAppService.Create(vendaRequest);
+            return Created("~https://localhost:7145/api/Payments", createdCustomer);
+        }
+        catch (BadRequestException e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(long id)
+    {
+        try
+        {
+            var customerFoundId = _vendaAppService.GetById(id);
+            return Ok(customerFoundId);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(long id, UpdateVendaRequest updateVendaRequest)
+    {
+        try
+        {
+            _vendaAppService.Update(id, updateVendaRequest);
+            return Ok();
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (BadRequestException e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
