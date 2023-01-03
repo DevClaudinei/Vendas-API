1cccf33fdc6b7c24441ce84b655a9dfb59a3c677
Claudinei Santos <claudinei.santos@warren.com.br>
2023-01-03 09:37:52 -0300

    feat: criacao de contexto para banco de dados

---
+++ src/Infraestructure.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infraestructure.Data.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+    { 
+        ChangeTracker.LazyLoadingEnabled = false;
+        ChangeTracker.AutoDetectChangesEnabled = false;
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.Load("Infrastructure.Data"));
+    }
+}
