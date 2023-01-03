8374220606fb7cc7429f090ff4cd4b4acaac4c36
Claudinei Santos <claudinei.santos@warren.com.br>
2023-01-03 09:31:04 -0300

    feat: criacao de mapeamento entre entidade de dominio e DTOs

---
+++ src/AppServices/Profiles/VendaProfile.cs
@@ -0,0 +1,14 @@
+﻿using Application.Models;
+using AutoMapper;
+using DomainModels.Entities;
+
+public class CustomerProfile : Profile
+{
+    public CustomerProfile()
+    {
+        CreateMap<Venda, VendaResult>()
+            .ForMember(x => x.IdVenda, opts => opts.MapFrom(source => source.Id));
+        CreateMap<UpdateVendaRequest, Venda>();
+        CreateMap<CreateVendaRequest, Venda>();
+    }
+}
